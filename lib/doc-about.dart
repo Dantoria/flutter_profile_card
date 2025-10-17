053cc2b5cb5f860e10acd33b63d01518a23d0eeb
Dantoria <zaidan.meiluano@gmail.com>
2025-10-17 09:19:31 +0700

    belajar flutter

---
+++ lib/doc-about.dart
@@ -0,0 +1,39 @@
+/** PENJELASAN
+ * 
+ * Struktur Flutter
+ * 
+ * 1.dart tool
+ * 2. idea
+ * 3. android
+ *  => for android configuration
+ *  => android library
+ * 4. build
+ *  => hasil compile apli
+ * 5.Ios
+ *  =>for Ios configuration
+ * 6. lib
+ *  =>place dart file later
+ *  =>sering digunakan
+ * 7. linux
+ *  =>for linux confi
+ * 8. macos
+ *  => macos confi
+ * 9. test
+ *  => for test apli
+ * 10. web => confi web
+ * 11. windows => confi windows
+ * 12.pubspec.yaml => 1. place for app confi 2. add libra 3. add asset
+ * 
+ * Di terminal
+ * function of "flutter pub get" => for synchronize libra in pubspec.yaml
+ * "flutter pub upg" => upg libra in yaml
+ * "flutter run" => to run
+ * "flutter clean" => 
+ * 
+ * libra from FLu can be seen in pub.dev
+ * intl, http, button, dll
+ * 
+ * id dart there are 2 statement
+ * 1. stateless => static
+ * 2. stateful => dynamic
+ */
