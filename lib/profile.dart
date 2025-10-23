555720e42ebb06cb124deed7fff36e7988ff0e03
Dantoria <zaidan.meiluano@gmail.com>
2025-10-23 13:51:49 +0700

    belajar container, padding

--- lib/profile.dart
+++ lib/profile.dart
@@ -19,16 +19,47 @@ class ProfileCard extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Text('Vladimir Guererro', style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            Row(
-              children: <Widget>[
-                Column(),
-                Column(),
-              ],
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.amberAccent,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.redAccent,
+                    spreadRadius: 5.0,
+                    blurRadius: 7.0,
+                    offset: Offset(0, 3),
+                  )
+                ]
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    /**
+                     * 
+                     */
+                    child: Text(
+                      'Name', 
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: 
+                        FontWeight.bold),
+                        ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                    ),
+                  Text(
+                    'Vladimir Guererro', style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
