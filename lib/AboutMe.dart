db471e3855a4686ebc235db21ca4b0241089a311
Dantoria <zaidan.meiluano@gmail.com>
2025-10-27 09:44:22 +0700

    Belajar image dan font dan singlechildscrollview

--- lib/AboutMe.dart
+++ lib/AboutMe.dart
@@ -12,70 +12,137 @@ class Aboutme extends StatelessWidget {
           ),
         backgroundColor: Colors.amberAccent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text('Zaidan'.toUpperCase(), style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    'assets/kereta.jpg',
+                  ),
                 ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Lorem Ipsum',
-                style: TextStyle(),
-                textAlign: TextAlign.justify,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.amberAccent,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2,
-                  )
+                Image.asset(
+                  'assets/profile.webp',
+                  width: 100,
+                  height: 100,
+                  ),
+                Text('Zaidan'.toUpperCase(), style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'JAK ARTA',
+        
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.redAccent,
-                        borderRadius: BorderRadius.circular(15),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Lorem Ipsum',
+                  style: TextStyle(),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amberAccent,
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    )
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Icon(
+                          Icons.android,
+                          color: Colors.white,
+                          size: 50,
+                        ),
                       ),
-                      child: Icon(
-                        Icons.android,
-                        color: Colors.white,
-                        size: 50,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Android Projects\'s', style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Android Projects\'s', style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text('10 APK')
-                        ],
+                            Text('10 APK')
+                          ],
+                        ),
                       ),
-                    ),
-                    Text('Experience')
-                  ],
+                      Text('Experience')
+                    ],
+                  ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    'schedule'.toUpperCase(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'JAK ARTA',
+                      fontSize: 18,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text('Belajar',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'JAK ARTA'
+                          ),
+                          ),
+                        Icon(Icons.timer),
+                        Text(
+                          '7.30 - 14.30'
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Membaca',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'JAK ARTA'
+                          ),
+                        ),
+                        Icon(
+                          Icons.book,
+                          size: 30,
+                        ),
+                        Text(
+                          '20.00 - 21.00',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'JAK ARTA'
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
